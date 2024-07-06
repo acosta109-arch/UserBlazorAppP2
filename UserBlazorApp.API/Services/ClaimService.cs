4318731df00b05caf204c56568cf84c33f2b08e5
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-07-06 09:32:50 -0400

    Agregando el claimService.

---
+++ UserBlazorApp.API/Services/ClaimService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserBlazorApp.API.DAL;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Services;
+
+public class ClaimService(UsersDbContext Contexto) : IApiService<AspNetRoleClaims>
+{
+	public async Task<List<AspNetRoleClaims>> GetAllAsync()
+	{
+		return await Contexto.AspNetRoleClaims.ToListAsync();
+	}
+
+	public async Task<AspNetRoleClaims> GetByIdAsync(int id)
+	{
+		return await Contexto.AspNetRoleClaims.FindAsync(id);
+	}
+
+	public async Task<AspNetRoleClaims> AddAsync(AspNetRoleClaims rol)
+	{
+		Contexto.AspNetRoleClaims.Add(rol);
+		await Contexto.SaveChangesAsync();
+		return rol;
+	}
+
+	public async Task<bool> UpdateAsync(AspNetRoleClaims rol)
+	{
+		Contexto.Entry(rol).State = EntityState.Modified;
+		return await Contexto.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> DeleteAsync(int id)
+	{
+		var rol = await Contexto.AspNetRoleClaims.FindAsync(id);
+		if (rol == null)
+			return false;
+
+		Contexto.AspNetRoleClaims.Remove(rol);
+		return await Contexto.SaveChangesAsync() > 0;
+	}
+}
